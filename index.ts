44bd828608482821b2deb1eaf0ec7dfd43c5011a
Ashish Sharda <ashishjsharda@gmail.com>
2022-07-24 15:40:22 -0500

    update index js and package json

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
-"use strict";
-const firestName ="Rich"
+import express from 'express';
+const app = express();
+app.get('/',(req,res) => {
+    res.send("Hello")
+});
+
+const port=process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
