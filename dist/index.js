751e4048b0e5f6cc0fecdd720fa87c82286651ee
Ashish Sharda <ashishjsharda@gmail.com>
2022-07-24 15:46:16 -0500

    update index js and package json

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-const firestName = "Rich";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`));
